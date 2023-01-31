fdaee955864bd0c3011ce93d9a0cb26971060d75
Mohdyusufsaifi <mdfaizmd.yusuf@gmail.com>
2023-01-31 22:59:34 +0530

    Initial commit

--- controllers/mail.js
+++ controllers/mail.js
@@ -22,7 +22,7 @@ exports.sendEmail=(req, res) => {
         if (error) {
             console.log(error);
         } else {
-            res.send("")
+            res.status(200).send(`<script>window.location.assign("http://localhost:3000/Contact")</script>`)
         }
     })
 };
